7809625b092dc20b1d82af7ea09fdd4422da5b4c
Fyëryn <67385290+M4rbleL1ne@users.noreply.github.com>
2024-10-30 16:38:21 +0100

    Forgot to change version in the code

--- source/LBMergedModsPlugin.cs
+++ source/LBMergedModsPlugin.cs
@@ -18,7 +18,7 @@ using Fisobs.Sandbox;
 
 namespace LBMergedMods;
 
-[BepInPlugin("lb-fgf-m4r-ik.modpack", "LB Merged Mods", "1.0.8"), BepInDependency("io.github.dual.fisobs")]
+[BepInPlugin("lb-fgf-m4r-ik.modpack", "LB Merged Mods", "1.1.0"), BepInDependency("io.github.dual.fisobs")]
 public sealed class LBMergedModsPlugin : BaseUnityPlugin
 {
     public static AssetBundle? Bundle;
